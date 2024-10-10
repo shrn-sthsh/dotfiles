b63c10c0132dfb44687b454e73f6a474cf721d24
Sharan Sathish <shrnsthsh@gmail.com>
2024-10-10 15:46:26 -0400

    Make waybar launch properly for multiple screens | change CPU temperature to be in Fahrenheit

--- config/waybar/launch.sh
+++ config/waybar/launch.sh
@@ -3,8 +3,12 @@
 # Terminate already running bar instances
 killall -q waybar
 
-# Wait until the processes have been shut down
+# Wait until the processes have been killed
 while pgrep -x waybar >/dev/null; do sleep 1; done
+
+# Temporaily double the number of file descriptors 
+# allowed to be spawned by terminal
+ulimit -n $((2 * $(ulimit -n)))
 
 # Launch waybar
 waybar &
