d50122ddbdfed3d940255c714e913ae6d3d94791
shrn-sthsh <103279462+shrn-sthsh@users.noreply.github.com>
2024-10-18 13:49:46 -0400

    Hugging face utility

---
+++ bash/func/huggingface.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+function hf()
+{
+  if [ "$1" == "key" ]; then
+    if [ "$2" == "-v" ] || [ "$2" == "--verbose" ]; then
+      unzip $HOME/Projects/hf.zip hfkey && \
+        cat hfkey && rm hfkey
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+      unzip $HOME/Projects/hf.zip hfkey && \
+        cat hfkey | pbcopy && \
+        rm hfkey
+    elif [ "$OSTYPE" == "linux-gnu" ]; then
+      if [[ $(tty) =~ "/dev/tty" ]]; then
+        unzip $HOME/Projects/hf.zip hfkey && \
+          cat hfkey && rm hfkey
+      elif [ "$XDG_SESSION_TYPE" == "wayland" ]; then
+        unzip $HOME/Projects/hf.zip hfkey && \
+          cat hfkey | wl-copy && \
+          rm hfkey 
+      else 
+        zip $HOME/Projects/hf.zip hfkey && \
+          cat hfkey | xsel --clipboard && \
+          rm hfkey
+      fi
+    fi
+  else
+    echo "ERROR: Unknown subcommand"
+  fi
+}
