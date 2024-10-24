2b7acebfc30d4964cb033e7dada532ff7c4546f7
shrn-sthsh <103279462+shrn-sthsh@users.noreply.github.com>
2024-10-24 18:14:18 -0400

    Give time for bless command in OSX boot even without reboot

--- boot/osx-boot.sh
+++ boot/osx-boot.sh
@@ -14,16 +14,12 @@ if [ "$sudoer" = false ]; then
   return 1
 fi
 
-# Get the current boot volume
+# Get the current boot volume and device identifier
 current_volume=$(bless --info --getBoot)
-
-# Get the current boot volume device identifier
 current_device=$(bless --info --getBoot | sed 's/.*\(\/dev\/[^ ]*\).*/\1/')
-
-# Translate the device identifier to a volume name
 current_volume=$(diskutil info "$current_device" | grep "Volume Name:" | awk '{for (i=3; i<=NF; i++) printf "%s ", $i; print ""}' | awk '{$1=$1};1')
 
-# Use an array to store bootable volumes
+# Collect bootable volumes
 boot_volumes=()
 while IFS= read -r volume; do
   # Check if the volume name does not contain "Data"
@@ -32,7 +28,7 @@ while IFS= read -r volume; do
 fi
 done < <(ls /Volumes)
 
-# Display the volumes in a numbered list and let the user select
+# Display the volumes
 echo "Available Bootable Volumes:"
 for i in "${!boot_volumes[@]}"; do
   volume=${boot_volumes[$i]}
@@ -54,13 +50,27 @@ fi
 selected_volume=${boot_volumes[$((choice-1))]}
 VOLUME_PATH="/Volumes/$selected_volume"
 
-# Use the bless command to set the boot volume
-echo -ne "\nSetting boot volume to $selected_volume... "
+# Set the boot volume with bless
+set_message="\nSetting boot volume to $selected_volume"
+echo -ne "$set_message"
 mkfifo passwordpipe
 bless --mount "$VOLUME_PATH" --setBoot < passwordpipe &
 echo "" > passwordpipe
 rm passwordpipe
-echo "done!"
+
+# Overwrite sudo password ask when already given macOS bug
+sleep 0.00001
+printf "\r%$(( $(whoami | wc -c) + 14 + ${#set_message}))s"
+stty sane
+stty echo
+
+# Countdown to give bless time
+echo -ne "\rSetting boot volume to $selected_volume"
+for i in {1..3}; do
+  echo -n "."
+  sleep 1
+done
+echo -n " done!"
 
 # Reboot
 if [ "$1" == "-r" ]; then
