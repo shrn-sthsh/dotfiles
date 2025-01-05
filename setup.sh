6a04f7c738c3b79dc3269cd89cf79782a670de27
Sharan Sathish <shrnsthsh@gmail.com>
2025-01-04 19:18:20 -0500

    Use loop-based relinking rather than rsync

--- setup.sh
+++ setup.sh
@@ -2,66 +2,90 @@
 
 
 # top level directories and paths
-SOURCE_DIR=$(pwd)
-TARGET_DIR="$HOME/.dotfiles"
+CURRENT_DIR=$(pwd)
+PROJECT_DIR="$HOME/.dotfiles"
+IGNORE_FILE="$PROJECT_DIR/packages/.gitignore"
+CONFIG_DIR="$HOME/.config"
 BACKUP_DIR="$HOME/.backup"
-GIT_IGNORE="$TARGET_DIR/packages/.gitignore"
 
 
-# Ensure correct placement of dotfiles
-if [ "$SOURCE_DIR" != "$TARGET_DIR" ]; then
+## Ensure dotfiles' root at home
+# check if already correct
+if [ "$CURRENT_DIR" != "$PROJECT_DIR" ]; then
   if [[ $- != *i* ]]; then
     echo "NOTE:   Moving repository to $HOME"
   fi
 
-  mv "$SOURCE_DIR" "$TARGET_DIR"
+  mv "$CURRENT_DIR" "$PROJECT_DIR"
 fi
 
 
-# Backup of current configurations
-if ! [ -d "$BACKUP_DIR" ]; then
-  mkdir -p "$BACKUP_DIR"
+## Backup all configurations and apply new ones
+# make backup dotfile directory at root
+if ! [ -d "$BACKUP_DIR" ] || ! [ -d "$BACKUP_DIR/.config" ]; then
+  mkdir -p "$BACKUP_DIR/.config"
 fi
 
+# backup configurations and link non-overridden ones to new config directory
+if [ -d "$CONFIG_DIR" ]; then
 
-# Copy non-overidden configurations
-if [ -d "$HOME/.config" ]; then
+  # set up git ignore file
+  if ! [ -f "$IGNORE_FILE" ]; then
+    if ! [ -d $(dirname "$IGNORE_FILE") ]; then
+      mkdir -p $(dirname "$IGNORE_FILE")
+    fi
+
+    touch "$IGNORE_FILE"
+  fi
+  if [ -s "$IGNORE_FILE" ]; then
+    : > "$IGNORE_FILE"
+  fi
+  echo "# Ignore configurations not provieded by dotfiles \$HOME/.config" >> "$IGNORE_FILE"
+
+  # backup all configurations files
   if [[ $- != *i* ]]; then
     echo "STATUS: Copying over existing configurations not set by dotfiles"
   fi
 
-  # move over non-overrided files
-  if ! [ -d "$TARGET_DIR/logs" ]; then
-    mkdir -p "$TARGET_DIR/logs"
-  fi
-  BACKUP_LOG="$TARGET_DIR/logs/setup_rsync.log"
-  rsync -a --ignore-existing --log-file="$BACKUP_LOG" "$HOME/.config/" "$TARGET_DIR/packages/"
-  if [[ $- != *i* ]]; then
-    echo "NOTE:   Saving synchronization log as $BACKUP_LOG"
-  fi
-
-  # set up git ignore file
-  if ! [ -f "$GIT_IGNORE" ]; then
-    if ! [ -d $(dirname "$GIT_IGNORE") ]; then
-      mkdir -p $(dirname "$GIT_IGNORE")
+  for directory in "$CONFIG_DIR"/* "$CONFIG_DIR"/.*; do
+    if ! [ -e "$directory" ]; then
+      continue
     fi
 
-    touch "$GIT_IGNORE"
-  fi
-  if [ -s "$GIT_IGNORE" ]; then
-    : > "$GIT_IGNORE"
-  fi
+    package=$(basename "$directory")
+    backup="$BACKUP_DIR/config/$package"
+    target="$PROJECT_DIR/packages/$package"
 
-  # update .gitignore with copied packages
-  if [[ $- != *i* ]]; then
-    echo -e "STATUS: Updating git ignore with non-overidden packages\n"
-  fi 
-  echo "# Ignore configurations not provieded by dotfiles \$HOME/.config" >> "$GIT_IGNORE"
-  grep '^>f' "$BACKUP_LOG" | awk '{print $2}' | sed 's/\//g' >> "$GIT_IGNORE"
+    # backup package configuration to .backup
+    mv "$directory" "$backup"
+
+    # link non-overidden packages and add to git ignore
+    if ! [ -e "$target" ]; then
+      ln -s "$backup" "$target"
+      echo "/$package" >> "$GITIGNORE_FILE"
+    fi
+  done
+fi
+
+# clean up .config
+if [ -e "$CONFIG_DIR" ]; then
+  if [ -z "$(ls -A "$CONFIG_DIR")" ]; then
+    rmdir "$CONFIG_DIR"
+
+  # move to a reaminder directory if .config is not empty
+  else 
+    REMAIN_DIR="$BACKUP_DIR/residual"
+    if ! [ -d "$REMAIN_DIR" ]; then
+      mkdir -r "$REMAIN_DIR"
+    fi
+
+    mv "$CONFIG_DIR" "$REMAIN_DIR"
+  fi
 fi
 
 
-# Backup directories and replace with symbolic links
+## Backup special files & directories and replace with symbolic links
+# backup target and repalce path with source
 function save_and_link() 
 {
   local source=$1
@@ -74,7 +98,7 @@ function save_and_link()
 
   # save non-link items and unlink links
   if ! [ -L "$target" ]; then
-    mv "$target" "$BACKUP_DIR"
+    mv "$target" "$BACKUP_DIR/$(echo "$target" | sed 's/^\.//')"
   else
     unlink "$target"
   fi
@@ -86,8 +110,7 @@ function save_and_link()
   ln -s "$source" "$target"
 }
 
-save_and_link "$TARGET_DIR/shell/run.sh"   "$HOME/.bashrc"
-save_and_link "$TARGET_DIR/packages"       "$HOME/.config"
+save_and_link "$PROJECT_DIR/shell/run.sh"  "$HOME/.bashrc"
 save_and_link "$HOME/.config/mozilla"      "$HOME/.mozilla"
 save_and_link "$HOME/.config/tmux"         "$HOME/.tmux"
 save_and_link "$HOME/.config/tmux/config"  "$HOME/.tmux.conf"
